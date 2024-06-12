5af312dc398dcb08384e6e6b4981eab540b02941
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-06-12 14:55:19 +0200

    Refactor: Making some changes and refactoring some codes

--- week-0/challenge-1.js
+++ week-0/challenge-1.js
@@ -5,23 +5,22 @@ const dolphinsScore = [44, 23, 71];
 const koalasScore = [65, 54, 49];
 
 const calcAverage = (arr) =>{
-    const sum = arr.reduce((a,b) => a + b)
-    return sum/arr.length
+    return arr.reduce((a,b) => a + b)/arr.length;
 }
 const checkWinner = (avgDolphins,avgKoalas) =>{
     if(avgDolphins >= avgKoalas *2 ){
-        return `Dolphins wins by (${avgDolphins} vs. ${avgKoalas})` 
+        return `Dolphins wins by (${avgDolphins} vs. ${avgKoalas})`; 
     } else if(avgKoalas >= avgDolphins * 2){
         return `Koalas wins by (${avgKoalas} vs. ${avgDolphins})` 
     } else {
-        return 'No team wins !'
+        return 'No team wins !';
     }
 }
-const avgDolphins = calcAverage(dolphinsScore)
-const avgKoalas = calcAverage(koalasScore)
+const avgDolphins = calcAverage(dolphinsScore);
+const avgKoalas = calcAverage(koalasScore);
 
-console.log('Game 1')
-console.log(checkWinner(avgDolphins,avgKoalas))
+console.log('Game 1');
+console.log(checkWinner(avgDolphins,avgKoalas));
 
 // TEST DATA 2
 
@@ -30,5 +29,5 @@ const koalasGame2 = [23,34,27];
 
 const avgDolphinsG2 = calcAverage(dolphinsGame2);
 const avgKoalasG2 = calcAverage(koalasGame2);
-console.log('Game 2')
+console.log('Game 2');
 console.log(checkWinner(avgDolphinsG2, avgKoalasG2));
