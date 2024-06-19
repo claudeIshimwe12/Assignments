c5ac250c416dfb9afde6b4ecf2e2b91370798de6
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-06-19 11:57:47 +0200

    delete button: add functionality for deleting a task

--- week-0/To-Do-App/script.js
+++ week-0/To-Do-App/script.js
@@ -8,12 +8,31 @@ addBtn.addEventListener("click", function () {
   const title = document.querySelector(".title-input").value;
   const description = document.querySelector(".description").value;
   const expDate = document.querySelector("#expDate").value;
+  // Check values
+  if (!title) {
+    alert("Please provide a task title");
+    return;
+  }
   const expireDate = expDate.replace("T", " ");
+
   tasks.push({
     title,
     description,
     expireDate,
   });
+  // Create Delete Buttons and add them to the task list
+  const deleteDiv = document.createElement("div");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("btn-secondary");
+  deleteBtn.classList.add("delete-btn");
+  const updateBtn = document.createElement("button");
+  updateBtn.textContent = "Update";
+  updateBtn.classList.add("btn-secondary");
+  updateBtn.classList.add("update-btn");
+
+  deleteDiv.append(deleteBtn);
+  deleteDiv.append(updateBtn);
 
   // Create elements and add the to the DOM Tree
 
@@ -31,23 +50,22 @@ addBtn.addEventListener("click", function () {
   div.append(titleElement);
   div.append(descirptionElement);
   div.append(expirationDateElement);
+  div.append(deleteDiv);
+  const taskList = document.querySelector(".task-list");
+  taskList.append(div);
 
-  document.querySelector(".task-list").append(div);
-  console.log(tasks);
+  document.querySelector(".title-input").textContent = "";
+  document.querySelector(".description").textContent = "";
+
+  // Imprementing Delete Button functionality
+
+  const buttons = document.querySelectorAll(".btn-secondary");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (e.target.classList.contains("delete-btn")) {
+        e.target.parentNode.parentNode.remove();
+      }
+    });
+  });
 });
-
-// tasks.forEach((task) => {
-//   const div = document.createElement("div");
-//   const title = document.createElement("h1");
-//   title.textContent = task.title;
-//   const descirption = document.createElement("p");
-//   descirption.textContent = task.description;
-//   const expirationDate = document.createElement("p");
-//   expirationDate.textContent = task.expDate;
-
-//   div.append(title);
-//   div.append(descirption);
-//   div.append(expirationDate);
-//   // console.log(div);
-//   document.querySelector(".task-list").append(div);
-// });
